203a38f1f37b3f504b8cfa0680869de4264bb69e
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-05-08 19:13:39 +0200

    axios and redux-promise added

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h2>React simple starter</h2>
+				{this.props.children}
+			</div>
+		);
+	}
 }
+
+export default App;
