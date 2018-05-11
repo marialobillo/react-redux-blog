c17082f3410d31a5859a1864134e9c04dc472abf
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-05-11 17:09:39 +0200

    navigation submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component{
-
+	static contextTypes = {
+		router: PropTypes.object
+	};
+	onSubmit(props){
+		this.props.createPost(props)
+			.then(() => {
+				this.context.router.push('/');
+			})
+	}
 	render(){
 		const { fields: { title, categories, content }, handleSubmit } = this.props;
 		console.log(title);
 		return(
-			<form onSubmit={handleSubmit(this.props.createPost)} >
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))} >
 				<h3>Create a New Post</h3>
 				<div className="form-group">
 					<div className={`form-group ${title.touched && title.invalid ? 'has-danger' : ''}`} >
@@ -34,6 +43,7 @@ class PostsNew extends Component{
 						</div>
 					</div>
 					<button type="submit" className="btn btn-primary">Submit</button>
+					<Link to="/" className="btn btn-warning">Cancel</Link>
 				</div>
 			</form>
 		);
